22ab72d8101fef370f04725b464bec5f31c4ee2b
jhimy-michel <jhimy0riv@gmail.com>
2020-05-12 21:18:57 -0400

    adding new components

---
+++ burger/src/components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./BurgerIngredients.css";
+
+function BurgerIngredients(props) {
+  let ingredient;
+
+  switch (props.type) {
+    case "bread-bottom":
+      ingredient = <div className="BreadBottom" />;
+      break;
+    case "bread-top":
+      ingredient = (
+        <div className="BreadTop">
+          <div className="Seeds1" />
+          <div className="Seed2" />
+        </div>
+      );
+      break;
+    case "meat":
+      ingredient = <div className="Meet" />;
+      break;
+    case "cheese":
+      ingredient = <div className="Cheese" />;
+      break;
+    case "bacon":
+      ingredient = <div className="Bacon" />;
+      break;
+    case "salad":
+      ingredient = <div className="Salad" />;
+      break;
+    default:
+      ingredient = null;
+  }
+
+  return ingredient;
+}
+BurgerIngredients.propTypes = {
+  type: PropTypes.string.isRequired,
+};
+export default BurgerIngredients;
--- burger/src/components/Layout/Layout.js
+++ burger/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
+import "./Layout.css";
 
 const Layout = (props) => (
   <Aux>
     <div>Toolbar, Sidedrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className="Content">{props.children}</main>
   </Aux>
 );
 
