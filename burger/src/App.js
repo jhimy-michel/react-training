c3eb20d31494a48874eae1ac9484cec5ddf99604
jhimy-michel <jhimy0riv@gmail.com>
2020-05-12 20:54:11 -0400

    adding initial layout

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
   );
 }
