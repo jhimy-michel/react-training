14e5763fd5455bf1b439fd927f08d5d00fe2f34a
jhimy-michel <jhimy0riv@gmail.com>
2020-06-14 18:45:05 -0400

    closing redux implementation

--- burger/src/containers/Orders/Orders.js
+++ burger/src/containers/Orders/Orders.js
@@ -5,27 +5,30 @@ import Order from "../../components/Order/Order";
 import axios from "../../axios-orders";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import { fetchOrders } from "../../store/actions/order";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Orders extends Component {
-  state = {
-    orders: [],
-    loading: true,
-  };
   componentDidMount() {
     this.props.onFetchOrders();
   }
   render() {
-    return (
-      <div>
-        {this.props.orders.map((order) => (
-          <Order
-            key={order.id}
-            ingredients={order.ingredients}
-            price={order.price}
-          />
-        ))}
-      </div>
-    );
+    let orders = <Spinner />;
+    if (this.props.loading) {
+      orders = <Spinner />;
+    } else {
+      orders = (
+        <div>
+          {this.props.orders.map((order) => (
+            <Order
+              key={order.id}
+              ingredients={order.ingredients}
+              price={order.price}
+            />
+          ))}
+        </div>
+      );
+    }
+    return orders;
   }
 }
 const mapToDispatchToProps = (dispatch) => {
@@ -36,6 +39,7 @@ const mapToDispatchToProps = (dispatch) => {
 const mapStateToProps = (state) => {
   return {
     orders: state.orders.orders,
+    loading: state.orders.loading,
   };
 };
 
