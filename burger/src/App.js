48c915c2d7cfeb5e8f16cd9e27ad461d459a7d03
jhimy-michel <jhimy0riv@gmail.com>
2020-06-20 22:32:33 -0400

    completing auth

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Route, withRouter } from "react-router-dom";
+import { Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import Layout from "./hoc/Layout/Layout";
@@ -12,22 +12,40 @@ import { authCheckState } from "./store/actions";
 
 function App(props) {
   useEffect(() => props.onTryAutoSignup());
-  return (
-    <div className="App">
-      <Layout>
+  let routes;
+  if (props.isAuthenticated) {
+    routes = (
+      <>
         <Route path="/" exact component={BurgerBuilder} />
         <Route path="/checkout" component={Checkout} />
         <Route path="/orders" component={Orders} />
+        <Route path="/logout" component={Logout} />
+        <Redirect to="/" />
+      </>
+    );
+  } else {
+    routes = (
+      <>
         <Route path="/auth" component={Auth} />
         <Route path="/logout" component={Logout} />
-      </Layout>
+        <Redirect to="/" />
+      </>
+    );
+  }
+  return (
+    <div className="App">
+      <Layout>{routes}</Layout>
     </div>
   );
 }
-
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
 const mapDispatchToProps = (dispatch) => {
   return {
     onTryAutoSignup: () => dispatch(authCheckState()),
   };
 };
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
